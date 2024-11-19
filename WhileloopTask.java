531f81974377b36377e5010e367210eae0840d39
RakecshKN <rakecshrk@gmail.com>
2024-11-19 17:20:25 +0530

    Add files via upload

---
+++ WhileloopTask.java
@@ -0,0 +1,14 @@
+package AllProgram;
+
+public class WhileloopTask {
+	
+	public static void main(String[] args) {
+		int i =0;
+		
+		while(i<100) {
+			i=i+10; // incrementing
+			System.out.println(i);
+		}
+	}
+
+}
