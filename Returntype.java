91405fa6a800505d9f9e7cc4a6fc0aebd130c4fa
RakecshKN <rakecshrk@gmail.com>
2024-11-19 17:45:53 +0530

    Add files via upload

---
+++ Returntype.java
@@ -0,0 +1,15 @@
+package AllProgram;
+
+public class Returntype {
+	
+	
+	static int addition(int a,int b) {
+		return(a+b);
+	}
+	
+	public static void main(String[] args) {
+		int Sum=addition(5,5);
+		System.out.println(Sum);
+	}
+
+}
