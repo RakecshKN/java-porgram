54b0efae2b74b7c078e2a48c6e3ff066ebe1719a
RakecshKN <rakecshrk@gmail.com>
2024-11-20 17:18:31 +0530

    Add files via upload

---
+++ MultiofTwo.java
@@ -0,0 +1,23 @@
+package AllProgram;
+
+import java.util.Scanner;
+
+public class MultiofTwo {
+	
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter the number:");
+		int n=obj.nextInt()	;
+		for( int i=2;i<=n;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("multplication of 2 between 2 to "+n+" :"+i);
+			}
+			
+		}
+
+     
+	}
+
+}
