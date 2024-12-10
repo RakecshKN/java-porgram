6cc6dbc4909be23542868be62611c5280218c83c
RakecshKN <rakecshrk@gmail.com>
2024-12-10 23:43:58 +0530

    Add files via upload

---
+++ ClassandObject.java
@@ -0,0 +1,12 @@
+package AllProgram;
+
+public class ClassandObject {
+	
+	String str="hello";//variable
+	public static void main(String[] args) {
+		//creating object using new keyword
+		ClassandObject obj=new ClassandObject();
+		System.out.println(obj.str);
+	}
+
+}
