3d4005f27b289330023237546ee75f30d1c336d7
RakecshKN <rakecshrk@gmail.com>
2024-12-10 23:46:59 +0530

    Add files via upload

---
+++ DoWhileJava.java
@@ -0,0 +1,14 @@
+package AllProgram;
+
+public class DoWhileJava {
+	public static void main(String[] args) {
+
+		int i=0;
+		do {
+			System.out.println(i);
+			i++;
+		
+		}while(i<=10);
+	}
+
+}
